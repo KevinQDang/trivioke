c4857a6d71ac7abf5e7a9c55719024f0ffbbe414
Kevin <kqd4ng@gmail.com>
2018-11-07 13:38:15 -0600

    changed all the query to function on mongo

--- server/index.js
+++ server/index.js
@@ -7,8 +7,18 @@ const session = require('express-session');
 const db = require('../db/mysql.js');
 const util = require('./helpers.js');
 
+// const io = require('socket.io')();
+
 const saltRounds = 10;
 const app = express();
+// io.on('connection', (client) => {
+//   client.on('subcribeToTimer', (interval) => {
+//     console.log('client on', interval);
+//     setInterval(() => {
+//       client.emit('timer', new Date());
+//     }, interval);
+//   });
+// });
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -21,15 +31,15 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-app.get('/songs', (req, res) => {
-  db.connection.query('select * from songs', (err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(results);
-    }
-  });
-});
+// app.get('/songs', (req, res) => {
+//   db.connection.query('select * from songs', (err, results) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(results);
+//     }
+//   });
+// });
 
 app.post('/songs', (req, res) => {
   util.getSongs(req, res);
@@ -37,7 +47,7 @@ app.post('/songs', (req, res) => {
 });
 
 app.post('/signup', (req, res) => {
-  util.createPassword(req, res, saltRounds);
+  util.createUser(req, res, saltRounds);
 });
 
 app.get('/login', (req, res) => {
@@ -45,6 +55,7 @@ app.get('/login', (req, res) => {
 });
 
 const port = 8080;
+// io.listen(8080);
 app.listen(process.env.PORT || port, () => {
   console.log(`listening on port ${process.env.PORT || port}`);
 });
